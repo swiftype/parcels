242ff3ca3bb4b3d8ce091954561cb9ff0a9368e0
Andrew Geweke <andrew@geweke.org>
2014-07-22 15:18:53 -0600

    Kill dead code; use #require_asset/#depend_on_asset depending on whether the widget in question actually contains any CSS at the moment or not.

--- lib/parcels/fortitude/widget_engine.rb
+++ lib/parcels/fortitude/widget_engine.rb
@@ -19,29 +19,6 @@ module Parcels
       def evaluate(context, locals, &block)
         widget_class = ::Fortitude::Widget.widget_class_from_file(context.pathname, :root_dirs => ::Parcels.view_paths)
         widget_class.try(:_parcels_widget_class_css) || ""
-
-=begin
-        css = widget_class.try(:_parcels_class_level_css)
-
-        css = nil
-        if widget_class
-          css = FortitudeParcels.css_from_fortitude_widget_class(klass)
-        end
-
-        if css && css.length > 0
-          raw_css = css.join("\n")
-
-          if (wrapper_css_class = klass.try(:parcels_wrapper_css_class))
-            raw_css = ".#{wrapper_css_class} {\n#{raw_css}\n}"
-          end
-
-          require 'sass'
-          engine = Sass::Engine.new(raw_css, :syntax => :scss)
-          engine.render
-        else
-          ""
-        end
-=end
       end
     end
   end
--- lib/parcels/sprockets.rb
+++ lib/parcels/sprockets.rb
@@ -1,3 +1,6 @@
+require 'active_support'
+require 'active_support/core_ext/string'
+
 require 'find'
 
 ::Sprockets::DirectiveProcessor.class_eval do
@@ -9,7 +12,13 @@ require 'find'
       Find.find(view_path) do |filename|
         # TODO: Add support for sidecar .css/.js files, etc.
         next unless File.extname(filename).strip.downcase == ".rb"
-        context.require_asset(filename)
+
+        klass = ::Fortitude::Widget.widget_class_from_file(filename, :root_dirs => ::Parcels.view_paths)
+        if klass && klass.respond_to?(:_parcels_widget_class_css) && (!(css = klass._parcels_widget_class_css).blank?)
+          context.require_asset(filename)
+        else
+          context.depend_on_asset(filename)
+        end
       end
     end
   end
