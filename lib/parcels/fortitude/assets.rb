48c1c74c1f94e1448c574247eb0ad9d41e6d5fc3
Andrew Geweke <andrew@geweke.org>
2014-09-28 17:41:40 -0700

    Add support for alongside files.

--- lib/parcels/fortitude/assets.rb
+++ lib/parcels/fortitude/assets.rb
@@ -11,6 +11,32 @@ module Parcels
       extend ActiveSupport::Concern
 
       module ClassMethods
+        def inherited(new_class)
+          super(new_class)
+
+          if respond_to?(:caller_locations, true) && false
+            locations = caller_locations(1, 1)
+            filename = locations.first.absolute_path
+            new_class._parcels_inherited_called_from(filename)
+          else
+            string = caller[0]
+            if string =~ /^([^:]+):\d+/
+              new_class._parcels_inherited_called_from($1)
+            else
+              raise "Parcels: #{new_class} inherited from #{self.name}, but caller string was unparseable: '#{string}'"
+            end
+          end
+        end
+
+        def _parcels_inherited_called_from(filename)
+          @_parcels_class_definition_files ||= [ ]
+          @_parcels_class_definition_files << filename
+        end
+
+        def _parcels_class_definition_files
+          @_parcels_class_definition_files ||= [ ]
+        end
+
         def _parcels_widget_outer_element_classes
           @_parcels_widget_outer_element_classes ||= begin
             out = [ ]
@@ -33,7 +59,25 @@ module Parcels
         end
 
         def _parcels_wrapping_css_class_required?
-          _parcels_inline_css_fragments.detect { |f| f.wrapping_css_class_required? }
+          (_parcels_inline_css_fragments.detect { |f| f.wrapping_css_class_required? }) ||
+            (_parcels_alongside_css_fragments.detect { |f| f.wrapping_css_class_required? })
+        end
+
+        def _parcels_alongside_css_fragments
+          @_parcels_alongside_css_fragments ||= _parcels_alongside_filenames.map do |filename|
+            ::Parcels::Fragments::CssFragment.new(File.read(filename), self, filename, 1, { }) if File.exist?(filename)
+          end.compact
+        end
+
+        def _parcels_alongside_filenames
+          out = [ ]
+
+          _parcels_class_definition_files.each do |filename|
+            filename = $1 if filename =~ /^(.*)\.rb$/i
+            out << "#{filename}.css"
+          end
+
+          out.select { |f| File.file?(f) }
         end
 
         def _parcels_add_wrapper_css_classes_to(attributes, wrapper_classes)
