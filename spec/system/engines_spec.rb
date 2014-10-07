b0b0d9538eadf182821b4b8714d4726e250894fa
Andrew Geweke <andrew@geweke.org>
2014-10-07 08:57:38 -0700

    Make sure we process engines in the correct order.

--- spec/system/engines_spec.rb
+++ spec/system/engines_spec.rb
@@ -83,5 +83,52 @@ describe "Parcels engines support", :type => :system do
         expect_erb_processed
       end
     end
+
+    it "should allow overriding #css_engines to specify engines"
+    it "should have :engines take precedence over #css_engines"
+    it "should properly inherit #css_engines from superclasses"
+  end
+
+  context "alongside files" do
+    it "should use engines specified by #css_engines"
+    it "should properly inherit #css_engines from superclasses"
+  end
+
+  it "should support multiple Sprockets engines (stringification, then ERb)" do
+    files {
+      file 'assets/basic.css', %{
+        //= require_parcels
+      }
+
+      widget 'views/my_widget' do
+        css %{
+          p { background-image: url("<\\\#{'%'}= 7 * 3 %>"); }
+        }, :options => { :engines => '.erb.str' }
+      end
+    }
+
+    expect_css_content_in('basic',
+      'views/my_widget.rb' => {
+        widget_scoped(:p) => "background-image: url(\"21\")"
+      })
+  end
+
+  it "should support multiple Sprockets engines (ERb, then stringification)" do
+    files {
+      file 'assets/basic.css', %{
+        //= require_parcels
+      }
+
+      widget 'views/my_widget' do
+        css %{
+          p { background-image: url("<\\\#{'%'}= 7 * 3 %>"); }
+        }, :options => { :engines => '.str.erb' }
+      end
+    }
+
+    expect_css_content_in('basic',
+      'views/my_widget.rb' => {
+        widget_scoped(:p) => "background-image: url(\"<%= 7 * 3 %>\")"
+      })
   end
 end
