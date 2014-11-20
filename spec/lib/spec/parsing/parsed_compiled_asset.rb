ad5c1b49c3347abb6d72a1fde7172710b2c63eb8
Andrew Geweke <andrew@geweke.org>
2014-11-20 08:50:01 -0800

    Very start of a framework for parsing assets in a much, much cleaner way.

---
+++ spec/lib/spec/parsing/parsed_compiled_asset.rb
@@ -0,0 +1,9 @@
+module Spec
+  module Parsing
+    class ParsedCompiledAsset
+      def initialize(raw_asset)
+        @raw_asset = raw_asset
+      end
+    end
+  end
+end
---
+++ spec/lib/spec/parsing/sprockets_asset.rb
@@ -0,0 +1,26 @@
+module Spec
+  module Parsing
+    class SprocketsAsset
+      def initialize(sprockets_env, asset_path)
+        @sprockets_env = sprockets_env
+        @asset_path = asset_path
+      end
+
+      def exists?
+        !! asset
+      end
+
+      def source
+        asset.source if asset
+      end
+
+      private
+      attr_reader :sprockets_env, :asset_path
+
+      def asset
+        @asset ||= (sprockets_env.find_asset(asset_path) || :none)
+        @asset unless @asset == :none
+      end
+    end
+  end
+end
