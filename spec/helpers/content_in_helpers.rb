8d3aaaf86a594eaab140d080611a700e800e9bb8
Andrew Geweke <andrew@geweke.org>
2014-11-29 17:25:59 -0800

    Kill some more old spec code.

--- spec/helpers/content_in_helpers.rb
+++ spec/helpers/content_in_helpers.rb
@@ -1,21 +1,6 @@
 require 'nokogiri'
 
 module ContentInHelpers
-  def subpath_to_widget_class(subpath)
-    subpath = $1 if subpath =~ %r{^(.*?)\.[^/]+$}i
-    full_path = File.join(this_example_root, subpath)
-    full_path += ".rb" unless full_path =~ /\.rb\s*$/i
-    Fortitude::Widget.widget_class_from_file(full_path, :root_dirs => this_example_root)
-  end
-
-  def rendered_widget_content(widget_subpath, args = { }, options = { })
-    widget_class = subpath_to_widget_class(widget_subpath)
-    html = widget_class.new(args).to_html
-    out = Nokogiri::HTML(html)
-    out = out.xpath('/html/body') if options.fetch(:extract_body_contents, true)
-    out
-  end
-
   def classes_from(node, xpath)
     elements = node.xpath(xpath)
 
