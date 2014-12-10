3c340e84c206c0c9b41032954acd3d10602495a2
Andrew Geweke <andrew@geweke.org>
2014-12-09 21:49:04 -0800

    And a little bit more cleanup still.

--- lib/parcels/rails/railtie.rb
+++ lib/parcels/rails/railtie.rb
@@ -6,29 +6,9 @@ module Parcels
       initializer :parcels, :after => :append_assets_path do |app|
         parcels = ::Rails.application.assets.parcels
 
-        # parcels.root = ::Rails.root
-        # parcels.widget_roots = ::Rails.application.config.autoload_paths
-
-
         ::ApplicationController.view_paths.map(&:to_s).each do |view_path|
           view_path = File.expand_path(view_path)
           parcels.add_widget_tree!(view_path)
-
-=begin
-          set_name = File.basename(view_path).to_sym
-          set_name = :all if view_path == File.expand_path(File.join(::Rails.root, 'app', 'views'))
-
-          if sets_defined[set_name]
-            $stderr.puts %{WARNING: Parcels could not define a Parcels set named #{set_name.inspect} for the Rails view path:
-  #{view_path}
-...because there is already a set named #{set_name.inspect}, which we defined for the Rails view path:
-  #{sets_defined[set_name]}.}
-          else
-            parcels.define_set!(set_name, view_path)
-            sets_defined[set_name] = view_path
-            $stderr.puts "Defining set: #{set_name.inspect} for #{view_path.inspect}"
-          end
-=end
         end
       end
     end
